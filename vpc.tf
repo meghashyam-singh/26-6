25ea5f409a88fa3613e02500b6ffa3f2511a5321
megha <megha@gmail.com>
2026-06-27 00:44:32 +0530

    26-6

--- vpc.tf
+++ vpc.tf
@@ -75,8 +75,8 @@ resource "aws_eip" "roboshop_eip" {
 }
 
 resource "aws_nat_gateway" "roboshop_nat" {
-    subnet_id = "aws_subnet.public[0].id"
-    allocation_id = "aws_eip.roboshop_eip.id"
+    subnet_id = aws_subnet.public[0].id
+    allocation_id = aws_eip.roboshop_eip.id
     depends_on = [ aws_internet_gateway.roboshop_igw ]
     tags = {
         Name = "${local.common_name}_nat"
