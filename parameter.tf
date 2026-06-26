ea66e29bc68efea3163cb25e5c713e3830003812
megha <megha@gmail.com>
2026-06-27 00:58:24 +0530

    26-6

--- parameter.tf
+++ parameter.tf
@@ -36,7 +36,7 @@ resource "aws_ssm_parameter" "nat_gateway_id" {
 
 resource "aws_ssm_parameter" "sg_ids" {
     count = length(var.sg_names)
-    name = "${lcoal.common_name}_${var.sg_names}_sg_id"
+    name = "${local.common_name}_${var.sg_names[count.index]}_sg_id"
     type = "String"
     value = aws_security_group.roboshop_sg[count.index].id
 }
