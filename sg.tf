eb2f045788b263f4f40e0868d1afce15a5ebdd9e
megha <megha@gmail.com>
2026-06-27 00:57:02 +0530

    26-6

---
+++ sg.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "roboshop_sg" {
+    count = length(var.sg_names)
+    name = "${local.common_name}_${var.sg_names[count.index]}_sg"
+    description = "roboshop project security groups"
+
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "tcp"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+
+    tags = {
+        Name = "${local.common_name}_${var.sg_names[count.index]}_sg"
+    }
+}
